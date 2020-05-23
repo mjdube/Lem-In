c368c4972721ff75207c708976668da72976bc23
Mlungisi <sirmj.dube@gmail.com>
2020-05-23 17:35:45 +0200

    Updated ft_create_graph function

--- lem-in.c
+++ lem-in.c
@@ -11,6 +11,8 @@ int                 main(int argc, char **argv)
     t_notes         *map;
     t_notes         *farm;
     t_map           *graph;
+    t_map           *room;
+    t_map           *link;
     t_block         *paths[1024];
     t_block         *links[1024];
 
@@ -21,7 +23,21 @@ int                 main(int argc, char **argv)
         if (ft_num_of_ants(farm) > 0)
         {
             ft_scan(&(*links), farm, &start, &end);
+
             graph = ft_create_graph(links);
+            link  = graph;
+            room = graph;
+            while (link)
+            {
+                ft_putendl(link->roomName);
+                link = link->links;
+            }
+            ft_putchar('\n');
+            while (room)
+            {
+                ft_putendl(room->roomName);
+                room = room->rooms;
+            }
         }
         else 
             ft_putendl("Error");
--- src/ft_create_graph.c
+++ src/ft_create_graph.c
@@ -2,29 +2,29 @@
 
 void		create_room_r(t_map **graph, t_block *link)
 {
-	t_map 	**temp;
+	t_map 	*temp;
 	t_map	*new_link;
 	t_map 	*new_room;
 
-	temp = graph;
+	temp = *graph;
 	new_link = NULL;
 	new_room = NULL;
-	if ((*temp) == NULL)
+	if ((*graph) == NULL)
 	{
-		(*temp) = (t_map *)malloc(sizeof(t_map));
+		(*graph) = (t_map *)malloc(sizeof(t_map));
 		new_link = (t_map *)malloc(sizeof(t_map));
 
-		(*temp)->roomName = link->nameRoom;
+		temp->roomName = link->nameRoom;
 		new_link->roomName = link->prev->nameRoom;
 		
-		(*temp)->links = new_link;
-		(*temp)->rooms = NULL;
+		temp->links = new_link;
+		temp->rooms = NULL;
 	}
 	else 
 	{
-		while ((*temp)->rooms != NULL)
+		while (temp->rooms != NULL)
 		{
-			(*temp) = (*temp)->rooms;
+			temp = temp->rooms;
 		}
 
 		new_room = (t_map *)malloc(sizeof(t_map));
@@ -34,37 +34,37 @@ void		create_room_r(t_map **graph, t_block *link)
 		new_link->roomName = link->prev->nameRoom;
 
 		new_room->rooms = NULL;
+		new_room->links = new_link;
+		temp->rooms = new_room;
 		new_link->links = NULL;
-
-		(*temp)->links = new_link;
-		(*temp)->rooms = new_room;
 	}
 }
 
 void		create_room(t_map **graph, t_block *link)
 {
-	t_map 	**temp;
+	t_map 	*temp;
 	t_map	*new_link;
 	t_map 	*new_room;
 
-	temp = graph;
+	temp = *graph;
 	new_link = NULL;
 	new_room = NULL;
-	if ((*temp) == NULL)
+	if (*graph == NULL)
 	{
-		(*temp) = (t_map *)malloc(sizeof(t_map));
+		*graph = (t_map *)malloc(sizeof(t_map));
 		new_link = (t_map *)malloc(sizeof(t_map));
 
-		(*temp)->roomName = link->nameRoom;
+		(*graph)->roomName = link->nameRoom;
 		new_link->roomName = link->next->nameRoom;
 
-		(*temp)->links = new_link;
-		(*temp)->rooms = NULL;
+		(*graph)->links = new_link;
+		(*graph)->rooms = NULL;
+		new_link->links = NULL;
 	}
 	else 
 	{
-		while ((*temp)->rooms != NULL)
-			(*temp) = (*temp)->rooms;
+		while (temp->rooms != NULL)
+			temp = temp->rooms;
 		new_room = (t_map *)malloc(sizeof(t_map));
 		new_link = (t_map *)malloc(sizeof(t_map));
 
@@ -74,53 +74,60 @@ void		create_room(t_map **graph, t_block *link)
 		new_room->rooms = NULL;
 		new_link->links = NULL;
 
-		(*temp)->links = new_link;
-		(*temp)->rooms = new_room;
+		temp->rooms = new_room;
+		new_room->links = new_link;
 	}
 }
 
 void		found_room_r(t_map **graph, t_block *links)
 {
-	t_map	**temp;
+	t_map	*temp;
 	t_map	*new_link;
 
-	temp = graph;
+	temp = *graph;
 	new_link = NULL;
-	while ((*temp)->rooms != NULL)
+	while (temp->rooms != NULL)
 	{
-		if (ft_strcmp((*temp)->roomName, links->nameRoom) == 0)
+		if (ft_strcmp(temp->roomName, links->nameRoom) == 0)
 			break ;
-		(*temp) = (*temp)->rooms;
+		temp = temp->rooms;
 	}
-	while ((*temp)->links != NULL)
-		(*temp) = (*temp)->links;
+	while (temp->links != NULL)
+		temp = temp->links;
+	
 	new_link = (t_map *)malloc(sizeof(t_map));
+	
 	new_link->roomName = links->prev->nameRoom;
+	
 	new_link->links = NULL;
-	(*temp)->links = new_link;
+	
+	temp->links = new_link;
 }
 
 void		found_room(t_map **graph, t_block *links)
 {
-	t_map	**temp;
+	t_map	*temp;
 	t_map	*new_link;
 
-	temp = graph;
+	temp = *graph;
 	new_link = NULL;
-	while ((*temp))
+	while (temp)
 	{
-		if (ft_strcmp((*temp)->roomName, links->nameRoom) == 0)
+		if (ft_strcmp(temp->roomName, links->nameRoom) == 0)
 			break ;
-		(*temp) = (*temp)->rooms;
+		temp = temp->rooms;
 	}
-	while ((*temp)->links != NULL)
+	while (temp->links != NULL)
 	{
-		(*temp) = (*temp)->links;
+		temp = temp->links;
 	}
 	new_link = (t_map *)malloc(sizeof(t_map));
+
 	new_link->roomName = links->next->nameRoom;
+	
 	new_link->links = NULL;
-	(*temp)->links = new_link;
+	
+	temp->links = new_link;
 }
 
 t_map       *ft_create_graph(t_block **links)
@@ -149,10 +156,10 @@ t_map       *ft_create_graph(t_block **links)
 	
 			create_room(&graph, link[i]);
 		}
-			// create_room(&graph, link[i]);
-		ft_putendl(graph->roomName);
-		ft_putendl(graph->links->roomName);
-		ft_putchar('\n');
+			// create_room(&*graph, link[i]);
+		// ft_putendl(graph->roomName);
+		// ft_putendl(*graph->links->roomName);
+		// ft_putchar('\n');
 		if (check_room(graph, link[i]->next) == 1)
 		{
 			found_room_r(&graph, link[i]->next);
@@ -161,10 +168,10 @@ t_map       *ft_create_graph(t_block **links)
 		{
 			create_room_r(&graph, link[i]->next);
 		}
-		ft_putendl(graph->roomName);
-		ft_putendl(graph->links->roomName);
+		// ft_putendl(*graph->roomName);
+		// ft_putendl(*graph->links->roomName);
 		
-		ft_putchar('\n');
+		// ft_putchar('\n');
 		i++;
     }
 	return (graph);
